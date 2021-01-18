6b0203a789d14e958143583606e162a23dc5edb2
Valentine <hzvalentine@gmail.com>
2021-01-18 12:40:24 +0100

    Update settings.py

--- stockradar/settings.py
+++ stockradar/settings.py
@@ -6,7 +6,6 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = ['*']
-DEBUG = True
 
 INSTALLED_APPS = [
     'core',
